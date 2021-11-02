e27ca68955ebdcfb3aca442e0bc70834a9867fa6
Jordan Leppert <jordanleppert@gmail.com>
2021-11-02 17:43:04 +0000

    Added options - timestamp format, flag to output total time passed

--- timestamps/cli.py
+++ timestamps/cli.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
 
+import argparse
 from datetime import *
 import sys
 
 
 def main():
-    format = '%Y-%m-%d %H:%M:%S '
+    parser = argparse.ArgumentParser(prog='timestamps')
+    parser.add_argument('-f', '--format', dest='format',  nargs='?', type=str, default='%Y-%m-%d %H:%M:%S ', help='Timestamp format')
+    parser.add_argument('-t', '--total', dest='total', action='store_true', default=False, help='Display total time taken at the end')
+    args = parser.parse_args()
+    timestamp_format = args.format
+    total = args.total
     
+    start = datetime.now()
     for line in sys.stdin:
-        timestamp = datetime.now().strftime(format)
+        timestamp = datetime.now().strftime(timestamp_format)
         print(timestamp + line.rstrip("\n"), flush=True)
-
+    
+    if total:
+        end = datetime.now()
+        diff = end - start
+        print(diff)
 
 if __name__ == '__main__':
     main()
